8a90f4c2d8c374d0e6c86a96305320456d686232
Thales Gomes <thaleshng@gmail.com>
2024-11-04 17:31:17 -0300

    Retirando função de testar API publicamente

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,22 @@ import { PrismaClient } from "@prisma/client";
 import swaggerUi from "swagger-ui-express";
 import swaggerDocument from "../swagger.json";
 
+const swaggerOptions = {
+    swaggerOptions: {
+        supportedSubmitMethods: [],
+    },
+};
+
 const port = 3000;
 const app = express();
 const prisma = new PrismaClient();
 
 app.use(express.json());
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(
+    "/docs",
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerDocument, swaggerOptions),
+);
 
 app.get("/tarefas", async (_, res) => {
     try {
